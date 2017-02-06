f25cb6f2720b738a7bb68d9395a0bb110a55b1bb
Ethan Brown <ethanbrown3@mail.weber.edu>
2017-02-06 16:03:10 -0700

    Changed contentpane to layout to BorderLayout. Set up TextArea for chat transcript also setup TextArea for input. Setup the action listener for the send button setup the keyListener for the ctrl+enter feature of the input textArea created a method for sendText().  Used to send the message in the input box to the transcript pane.

--- src/chat/ChatWindow.java
+++ src/chat/ChatWindow.java
@@ -6,11 +6,16 @@
  */
 package chat;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -22,11 +27,14 @@ import javax.swing.ScrollPaneConstants;
  *
  */
 public class ChatWindow extends JFrame {
-	
-	JPanel contentPane;
-	JTextArea chatPanel;
-	JScrollPane scroll;
-	
+
+	private JPanel contentPane;
+	private JTextArea chatPane;
+	private JTextArea chatInput;
+	private JPanel inputPanel;
+	private JScrollPane scroll;
+	private JButton send;
+
 	/**
 	 * 
 	 */
@@ -36,23 +44,67 @@ public class ChatWindow extends JFrame {
 	 * @throws HeadlessException
 	 */
 	public ChatWindow() throws HeadlessException {
-		
-		JPanel contentPane = new JPanel(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		chatPanel = new JTextArea("TEST TEST TEST TEST TEST TEST TEST TEST TEST\n TEST\n TEST TEST ");
-	    chatPanel.setEditable(false); 
-	    scroll = new JScrollPane(chatPanel);
-//	    scroll.setSize(40, 40);
-	    scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		
-	    
-	    contentPane.add(scroll);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(new Dimension(500, 400));
+		this.setResizable(true);
+
+		contentPane = new JPanel(new BorderLayout());
+
+		chatPane = new JTextArea(
+				"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.\n"
+						+ "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.\n");
+		chatPane.setEditable(false);
+		chatPane.setLineWrap(true);
+		chatPane.setWrapStyleWord(true);
+		scroll = new JScrollPane(chatPane);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		contentPane.add(scroll, "Center");
+
+		getContentPane().add(contentPane);
+
+		inputPanel = new JPanel(new FlowLayout());
+		chatInput = new JTextArea(0,25);
+		chatInput.setEditable(true);
+
+		send = new JButton("Send");
+		inputPanel.add(chatInput);
+		inputPanel.add(send);
+
+		send.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sendText();
+			}
+		});
+		chatInput.addKeyListener(new KeyListener(){
+		    @Override
+		    public void keyPressed(KeyEvent e){
+		        if(e.getKeyCode() == KeyEvent.VK_ENTER && e.getModifiers() == KeyEvent.CTRL_MASK) {
+		        sendText();
+		        }
+		    }
+
+		    @Override
+		    public void keyTyped(KeyEvent e) {
+		    }
+
+		    @Override
+		    public void keyReleased(KeyEvent e) {
+		    }
+		});
+
+		contentPane.add(inputPanel, "South");
+
 		contentPane.setVisible(true);
-		this.add(contentPane);
-		
-		
-		setSize(new Dimension(600, 600));
 		setVisible(true);
+	}
+	
+	private void sendText() {
+		String chatSend;
+		String userName = "Ethan";
+		chatSend = chatInput.getText();
+		chatPane.append(userName +": " + chatSend + "\n");
+		chatInput.setText("");
 	}
 
 	/**
